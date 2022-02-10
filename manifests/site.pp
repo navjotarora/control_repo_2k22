a3c7d2ffc8e83be68b03626e8ab23ab1c928bba8
navjotarora <46839000+navjotarora@users.noreply.github.com>
2022-02-10 09:26:45 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,6 @@
 node default {
-  file {'/root/README.txt':
-    ensure  => file,
-    content => 'This is a readme file',
+  file {'/root/README':
+    ensure => file,
+    content => 'This is a README file',
   }
 }
