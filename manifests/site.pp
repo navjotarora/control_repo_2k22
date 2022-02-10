4becd39cafeba1e1403b8c36027fb15a45224e90
navjotarora <46839000+navjotarora@users.noreply.github.com>
2022-02-10 09:41:56 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,12 @@
 node default {
-  file {'/root/README':
+  file {'/root/puppettestdir/README':
     ensure => file,
     content => 'This is a README file',
   }
 }
 
 node master.puppet.vm {
-  file {'/root/READMEE': 
+  file {'/root/puppettestdir/READMEE': 
     ensure  => file,
     content => "Welcome to ${fqdn}\nYay!\n",
   }
